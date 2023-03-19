a0dac05e3746a8d1ff3bd27ae4429ea7feadd183
burnick <arnelceledonio@gmail.com>
2023-03-19 08:05:56 +0800

    feat: validation for inputs
    - user-signup
    - create-user.input

--- src/auth/dto/user-signup.ts
+++ src/auth/dto/user-signup.ts
@@ -1,11 +1,12 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail } from 'class-validator';
+import { IsEmail, Length } from 'class-validator';
 @InputType()
 export class UserSignupInput {
   @IsEmail()
   @Field({ nullable: false })
   username: string;
 
+  @Length(5, 30)
   @Field({ nullable: false })
   password: string;
 }
--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,8 +1,9 @@
 import { InputType, Field } from '@nestjs/graphql';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsDate, Length } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
+  @Length(10, 20)
   @Field()
   name: string;
 
@@ -10,12 +11,14 @@ export class CreateUserInput {
   @Field({ nullable: false })
   email: string;
 
+  @Length(5, 30)
   @Field({ nullable: false })
   password: string;
 
   @Field({ nullable: true })
   address?: string;
 
+  @IsDate()
   @Field({ nullable: true })
   updatedAt?: Date;
 }
