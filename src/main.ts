7aa3f099f5e31f2885f30c8b1c53f21b0f666002
burnick <arnelceledonio@gmail.com>
2023-03-21 19:39:09 +0800

    fix: schema

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
     },
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(configService.getPort() ?? 3000);
 }
 bootstrap();
